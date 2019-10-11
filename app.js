1355b3dbd3b19e856c88a73eef9dd6cac2877644
Eloisa Borromeo <beloisa94@gmail.com>
2019-10-11 16:19:50 +0800

    added validation function

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ let email = document.getElementById('email');
 let errorMessage = document.createElement('p');
 errorMessage.classList.add('error-message');
 errorMessage.innerHTML = 'Please provide a valid email';
+
+let emailCheck = () => {
+    if (email.validity.valueMissing && !email.validity.valid || !email.validity.valueMissing && email.validity.valid) {
+        email.classList.add('valid');
+        email.classList.remove('invalid');
+        form.parentNode.removeChild(errorMessage);
+    } else if (!email.validity.valueMissing && !email.validity.valid) {
+        email.classList.add('invalid');
+        email.classList.remove('valid');
+        form.parentNode.appendChild(errorMessage);
+    }
+}
