a22f30d5dd0c2297f6637f0f1a91dba6f99d4e7d
Eloisa Borromeo <beloisa94@gmail.com>
2019-10-14 13:13:13 +0800

    form validation fixes

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ let errorMessage = document.createElement('p');
 errorMessage.classList.add('error-message');
 errorMessage.innerHTML = 'Please provide a valid email';
 
-let emailCheck = () => {
-    if (email.validity.valueMissing && !email.validity.valid || !email.validity.valueMissing && email.validity.valid) {
-        email.classList.add('valid');
+let resetForm = () => {
+    if (email.value === "" || email.validity.valid) {
         email.classList.remove('invalid');
+        email.classList.add('valid');
         form.parentNode.removeChild(errorMessage);
-    } else if (!email.validity.valueMissing && !email.validity.valid) {
+    } 
+}
+
+let submitCheck = () => {
+    if (email.validity.valueMissing && !email.validity.valid || !email.validity.valueMissing && !email.validity.valid) {
         email.classList.add('invalid');
         email.classList.remove('valid');
         form.parentNode.appendChild(errorMessage);
+    } else {
+        email.classList.add('valid');
+        email.classList.remove('invalid');
+        form.parentNode.removeChild(errorMessage);
+        form.removeAttribute("novalidate");
     }
 }
 
-email.addEventListener("input", (e) => { 
-    e.preventDefault(); 
-    emailCheck();
-    
+email.addEventListener("keyup", (e) => { 
+    e.preventDefault();
+    resetForm();
+})
+
+form.addEventListener("submit", (e) => {
+    submitCheck();
+    e.preventDefault();
 })
