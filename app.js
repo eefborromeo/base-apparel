553ed1fd897eb4c7cdf3fb5c5d3e48042b2b4472
Eloisa Borromeo <beloisa94@gmail.com>
2020-01-15 20:55:59 +0800

    let -> const, changed to querySelector

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-let form = document.getElementsByTagName('form')[0];
-let email = document.getElementById('email');
-let errorMessage = document.createElement('p');
+const form = document.querySelector('.email-form');
+const email = document.querySelector('input');
+const errorMessage = document.createElement('p');
 errorMessage.classList.add('error-message');
-errorMessage.innerHTML = 'Please provide a valid email';
+errorMessage.textContent = 'Please provide a valid email';
 
-let resetForm = () => {
-    if (email.value === "" || email.validity.valid) {
-        email.classList.remove('invalid');
-        email.classList.add('valid');
-        form.parentNode.removeChild(errorMessage);
-    } 
-}
+const resetForm = () => {
+  if (email.value === '' || email.validity.valid) {
+    email.classList.remove('invalid');
+    email.classList.add('valid');
+    form.parentNode.removeChild(errorMessage);
+  }
+};
 
-let submitCheck = () => {
-    if (email.validity.valueMissing && !email.validity.valid || !email.validity.valueMissing && !email.validity.valid) {
-        email.classList.add('invalid');
-        email.classList.remove('valid');
-        form.parentNode.appendChild(errorMessage);
-    } else {
-        email.classList.add('valid');
-        email.classList.remove('invalid');
-        form.parentNode.removeChild(errorMessage);
-        form.removeAttribute("novalidate");
-    }
-}
+const submitCheck = () => {
+  if (
+    (email.validity.valueMissing && !email.validity.valid) ||
+    (!email.validity.valueMissing && !email.validity.valid)
+  ) {
+    email.classList.add('invalid');
+    email.classList.remove('valid');
+    form.parentNode.appendChild(errorMessage);
+  } else {
+    email.classList.add('valid');
+    email.classList.remove('invalid');
+    form.parentNode.removeChild(errorMessage);
+    form.removeAttribute('novalidate');
+  }
+};
 
-email.addEventListener("keyup", (e) => { 
-    e.preventDefault();
-    resetForm();
-})
+email.addEventListener('keyup', e => {
+  e.preventDefault();
+  resetForm();
+});
 
-form.addEventListener("submit", (e) => {
-    submitCheck();
-    e.preventDefault();
-})
+form.addEventListener('submit', e => {
+  submitCheck();
+  e.preventDefault();
+});
